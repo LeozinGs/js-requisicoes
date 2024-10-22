8cd162d55f8bb884508177ffcc35f80bf34bfe6b
LeozinGs <lokinho_ddtank@hotmail.com>
2024-10-22 12:32:02 -0300

    Consome uma api mockada para colocar os videos na tela.

---
+++ js/mostrarVideos.js
@@ -0,0 +1,33 @@
+import { conectApi } from "./conectApi.js";
+
+const lista = document.querySelector('[data-lista]');
+
+function constroiCard(titulo, descricao, url, image) {
+    const video = document.createElement('li');
+    video.className = 'videos__item';
+    video.innerHTML = `
+            <iframe width="100%" height="72%" src="${url}"
+                title="${titulo}" frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen></iframe>
+            <div class="descricao-video">
+                <img src="${image}">
+                <h3>${titulo}</h3>
+                <p>${descricao}</p>
+            </div>
+            `;
+    
+    return video;
+}
+
+async function listaVideos() {
+    const listaApi = await conectApi.listaVideos();
+    listaApi.forEach(element => lista.appendChild(constroiCard(
+        element.titulo,
+        element.descricao,
+        element.url,
+        element.imagem
+    )));
+}
+
+listaVideos();
